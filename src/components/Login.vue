87603acc10c4b8edafca437a1f2347c1ac68d824
CaoKuo <1150729044@qq.com>
2020-05-24 07:43:45 +0800

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <el-form ref="loginFormRef" label-width="0px" :model="loginForm" :rules="rules" class="login_form">
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="el-icon-user"
+            v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="el-icon-lock"
+            v-model="loginForm.password"
+            type="password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login()">登录</el-button>
+          <el-button type="info" @click="resetForm()">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入用户密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginFormRef.validate(valid => {
+        if (!valid) return
+        // this.$http.post('login', this.loginForm)
+        if (this.loginForm.username === 'admin' && this.loginForm.password === '123456') {
+          this.$message.success({ message: '登录成功', duration: 1000 })
+          window.sessionStorage.setItem('token', 'adesfintwering123456789')
+          this.$router.push('/home')
+        } else {
+          this.$message.error({ message: '登录失败', duration: 2000 })
+        }
+      })
+    },
+    resetForm () {
+    //   console.log(this)
+      this.$refs.loginFormRef.resetFields()
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  height: 100%;
+  background-color: #2b4b6b;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #ffffff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eeeeee;
+    border-radius: 50%;
+    padding: 10px;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #ffffff;
+    box-shadow: 0 0 10px #ddd;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eeeeee;
+    }
+  }
+}
+
+.login_form {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
